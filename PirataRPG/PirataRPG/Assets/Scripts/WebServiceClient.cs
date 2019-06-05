bd8169bf8f83e80645642a4812d7c4fe1db4290f
Stanley De Moya <Standmv13@gmail.com>
2019-06-05 18:03:25 -0400

    Consumiendo GET desde un servicio web

---
+++ PirataRPG/PirataRPG/Assets/Scripts/WebServiceClient.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+
+public class WebServiceClient : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(SendRequest("https://my.api.mockaroo.com/my_saved_schema.json?key=6b179840"));
+        }
+    }
+
+    IEnumerator SendRequest(string url)
+    {
+        UnityWebRequest unityWebRequest = UnityWebRequest.Get(url);
+        yield return unityWebRequest.SendWebRequest();
+
+        Debug.Log(unityWebRequest.downloadHandler.text);
+    }
+}
